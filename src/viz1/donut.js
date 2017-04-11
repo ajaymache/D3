0c8a0e8506f8098ba551f804d4afe4145ba146f8
Ajay Mache <aj@Ajays-MacBook-Pro.local>
2017-04-10 21:53:26 -0400

    some updates

--- src/viz1/donut.js
+++ src/viz1/donut.js
@@ -1,8 +1,10 @@
 var data = [{
   values: [16, 15, 12, 6, 5, 4, 42],
   labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World' ],
+  textinfo: 'percent',
+  textposition: 'outside',
   domain: {
-    x: [0, .48]
+    x: [0, 0.45]
   },
   name: 'GHG Emissions',
   hoverinfo: 'label+percent+name',
@@ -11,9 +13,9 @@ var data = [{
 },{
   values: [27, 11, 25, 8, 1, 3, 25],
   labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World' ],
-  text: 'CO2',
-  textposition: 'inside',
-  domain: {x: [.52, 1]},
+  textinfo: 'percent',
+  textposition: 'outside',
+  domain: {x: [0.55, 1]},
   name: 'CO2 Emissions',
   hoverinfo: 'label+percent+name',
   hole: .75,
@@ -25,25 +27,29 @@ var layout = {
   annotations: [
     {
       font: {
-        size: 14
+        size: 18
       },
       showarrow: false,
       text: 'GHG',
-      x: 0.17,
-      y: 0.5
+      x: 0.20,
+      y: 'auto'
     },
     {
       font: {
-        size: 14
+        size: 18
       },
       showarrow: false,
       text: 'CO2',
-      x: 0.82,
-      y: 0.5
+      x: '0.80',
+      y: 'auto'
     }
   ],
-  height: 600,
-  width: 780
+  height: 500,
+  width: 1125,
+  legend:
+    {
+      orientation : 'h'
+    }
 };
 
-Plotly.newPlot('myDiv', data, layout);
+Plotly.newPlot('myDiv', data, layout,{displayModeBar: false});
